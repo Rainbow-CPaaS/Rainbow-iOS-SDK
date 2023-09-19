dab638988cf5a655a8b2cb651f037df76df8a6a4
Jenkins Mac mini M1 <ios_dev_team@al-enterprise.com>
2023-09-19 10:57:41 +0200

    Update for Rainbow SDK 2.9.0 LTS

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.8.0"
-let checksum = "2c44669df000d9dd36b9905e18cdd182b34b56abb576fa9e152bf987f8d1d16a"
+let version = "2.9.0"
+let checksum = "bb4b6c2c7f3937b40ede9b805be87da9d6c83c992484148eec3c3af5d1933724"
 
 let package = Package(
     name: "RainbowSDK",
